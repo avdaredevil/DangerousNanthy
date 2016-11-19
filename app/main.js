fda2964a19e7d8f3e45b32a3cb42be509b0e03f7
Apoorv Verma[AP] <avdaredevil@gmail.com>
2016-11-19 04:46:24 -0500

    For [28cdfdd480bf5fca306fa3fc0d584f4ff56a4631] I also needed to do Math abs, for whatever reason

--- app/main.js
+++ app/main.js
@@ -18,7 +18,7 @@ const nanthy = {
         nanthy.sprite.reset(xc,yc)
         nanthy.sprite.scale.x = Math.abs(nanthy.sprite.scale.x)
         nanthy.direction = "right"
-        ;[10,100,1000].forEach(t => sleep(t).then(_=>{nanthy.sprite.body.width = nanthy.sprite.width/3}));
+        ;[10,100,1000].forEach(t => sleep(t).then(_=>{nanthy.sprite.body.width = Math.abs(nanthy.sprite.width/3)}));
         //properties, duration, ease, autoStart, delay, 4, yoyo
         level.bornTween && level.bornTween.stop()
         level.bornTween = game.add.tween(nanthy.sprite).to({alpha: 0}, 400, Phaser.Easing.Bounce.InOut, true, 0, -1, true)
